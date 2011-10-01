7f2cb49908715b134d93d7a452c6082188b0fb30
Dan F-M <dan@danfm.ca>
2011-09-30 21:18:05 -0400

    added mongo fucking

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python
 # encoding: utf-8
+
 import os
+
 from flask import Flask
+import pymongo
+
+from models import Eff, collection
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/<new_eff>/")
+def do_eff(new_eff):
+    if new_eff != "favicon.ico":
+        eff = Eff(new_eff)
+        eff.inc()
+        eff.save()
+    return "<br>".join(["Fuck %s! (%d)"%(doc['eff'], doc['count'])
+        for doc in collection.find(
+            fields={'eff': 1, 'count': 1}).sort('count', pymongo.DESCENDING)])
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
---
+++ models.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from datetime import datetime
+
+import pymongo
+
+import config
+
+db = pymongo.Connection(config.SERVER, config.PORT)[config.DATABASE]
+db.authenticate(config.USERNAME,config.PASSWORD)
+
+collection = db.eff
+collection.ensure_index('eff')
+collection.ensure_index('count')
+collection.ensure_index('date_modified')
+
+class Eff(object):
+    def __init__(self, eff):
+        self._doc = collection.find_one({'eff': eff})
+        if self._doc is None:
+            self._doc = {'eff': eff, 'count': 0, 'date_created': datetime.now()}
+
+    def inc(self):
+        self._doc['date_modified'] = datetime.now()
+        self._doc['count'] += 1
+
+    def save(self):
+        self._doc['_id'] = collection.save(self._doc)
+
