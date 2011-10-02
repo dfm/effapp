c5c2e7721d9c35803725c1dbca013e14717513a2
Dan F-M <dan@danfm.ca>
2011-10-01 22:35:14 -0400

    adding google URL shortening

--- models.py
+++ models.py
@@ -2,6 +2,8 @@
 # encoding: utf-8
 
 from datetime import datetime
+import urllib2
+import json
 
 import pymongo
 
@@ -15,11 +17,19 @@ collection.ensure_index('eff')
 collection.ensure_index('count')
 collection.ensure_index('date_modified')
 
+google_url = "https://www.googleapis.com/urlshortener/v1/url?key=%s"%(config.GAPIKEY)
+
 class Eff(object):
     def __init__(self, eff):
         self._doc = collection.find_one({'eff': eff})
         if self._doc is None:
             self._doc = {'eff': eff, 'count': 0, 'date_created': datetime.now()}
+        if 'short_url' not in self._doc:
+            data = {"longUrl": '/'.join([config.BASEURL, eff])}
+            headers = {"Content-Type": "application/json"}
+            req = urllib2.Request(google_url, json.dumps(data), headers)
+            res = json.loads(urllib2.urlopen(req).read())
+            self._doc['short_url'] = res['id']
 
     def __getattr__(self, name):
         return self._doc[name]
