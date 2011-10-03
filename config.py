e0c567a2b01414bee107f2b557746f9b06f01377
Dan F-M <dan@danfm.ca>
2011-10-02 20:49:41 -0400

    removing config.py history

---
+++ config.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import os
+
+e = os.environ
+
+DEBUG      = False
+SECRET_KEY = e['SECRET_KEY']
+USERNAME   = e['MONGO_USER']
+PASSWORD   = e['MONGO_PASS']
+DATABASE   = e['MONGO_DB']
+SERVER     = e['MONGO_SERVER']
+PORT       = int(e['MONGO_PORT'])
+GAPIKEY    = e['GAPIKEY']
+BASEURL    = e['BASEURL']
+
