4bdfa345ee5b1f2f31784a27a1dec7167984247f
Micha Gorelick <mynameisfiber@gmail.com>
2011-10-10 22:10:36 -0400

    Look at me.. I'm pymongo and my caching is already good.  bah.

--- database.py
+++ database.py
@@ -16,6 +16,9 @@ class Database(object):
     self.collection.ensure_index('date_modified')
     self._cache = {}
 
+  def __del__(self):
+    self.db.logout()
+
   def save(self, item):
     return self.collection.save(item)
 
@@ -35,28 +38,11 @@ class Database(object):
   def get_access_times(self, item):
     return self.collection.find_one({"eff":item}, fields={'date_access' : 1 })
 
-  def cache_list_request(field):
-    def wrap(func):
-      def cache(self, limit=10, *args, **kwargs):
-        try:
-          if self._cache[field]["limit"] <= limit:
-            return self._cache[field]["data"][:limit]
-        except KeyError:
-          self._cache[field] = {"limit":0, "data":None}
-        finally:
-          self._cache[field]["limit"] = limit
-          self._cache[field]["data"]  = func(self, limit, *args, **kwargs)
-          return self._cache[field]["data"]
-      return cache
-    return wrap
-
-  @cache_list_request("popular")
   def get_popular(self, limit=10):
     return self.collection.find(fields={'eff': 1, 'count': 1}) \
                           .sort([('count', pymongo.DESCENDING),('eff', pymongo.ASCENDING)]) \
                           .limit(limit)
 
-  @cache_list_request("recent")
   def get_recent(self, limit=10):
     return self.collection.find(fields={'eff': 1, 'date_modified': 1}) \
                           .sort([('date_modified', pymongo.DESCENDING)]) \
@@ -65,18 +51,7 @@ class Database(object):
 
 
 if __name__ == "__main__":
-  d = Database()
   from time import time
-
-  start = time()
-  popular = d.get_popular(10)
-  print "Getting populars with DB call took %f s"%(time()-start)
-  start = time()
-  popular = d.get_popular(10)
-  print "Getting populars with cache took %f s"%(time()-start)
-  start = time()
-  popular = d.get_popular(2)
-  print "Getting populars with cache took %f s"%(time()-start)
-
+  d = Database()
   print "Popular:", [x["eff"] for x in d.get_popular(2)]
   print "Recent: ", [x["eff"] for x in d.get_recent(10)]
